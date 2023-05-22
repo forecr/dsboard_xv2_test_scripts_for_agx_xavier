1119adb8bd21b910a4959338f9e7579919fa0e55
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-05-22 09:59:13 +0300

    Interrupt functions added in CANBus & digital-in script files

--- JetPack-4/test_digital_in0_agx.sh
+++ JetPack-4/test_digital_in0_agx.sh
@@ -7,6 +7,10 @@ fi
 sudo echo 240 > /sys/class/gpio/export
 sudo echo in > /sys/class/gpio/gpio240/direction
 
+trap interrupt_func INT
+interrupt_func() {
+	sudo echo 240 > /sys/class/gpio/unexport
+}
+
 watch -n 0.1 sudo cat /sys/class/gpio/gpio240/value
 
-sudo echo 240 > /sys/class/gpio/unexport
